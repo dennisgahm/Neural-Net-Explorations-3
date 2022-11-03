99dc2ca9cf8da216d9f53e01590bb7fa8be49ede
Dennis Gahm <dennisgahm@hotmail.com>
2022-11-03 19:19:10 +1000

    Created basic perceptronNode

---
+++ Neural Net Explorations 3/IfStatement.vb
@@ -0,0 +1,13 @@
+﻿Public Class IfStatement
+    Public HashTable1 As Hashtable
+    'A list of keys of situations and their resulting true or falsity
+    'For example, "ballHitsStick" -> true
+    'sky is blue -> true
+    'There is a confoundation - how to maintain a list of variable key names
+    'we need to give the computer understanding of language
+    ' synonyms for variable names map to the same true or falsity
+
+    Public Sub AddKeyValue(ByVal key As String, ByVal value As Boolean)
+        HashTable1.Add(key, value)
+    End Sub
+End Class
---
+++ Neural Net Explorations 3/PerceptronNode.vb
@@ -0,0 +1,34 @@
+﻿Public Class PerceptronNode
+    'percentages?
+    Dim IfStatements As ArrayList
+
+    Public Sub AddIfStatement(ByVal situation As IfStatement)
+        IfStatements.Add(situation)
+    End Sub
+
+    Public Function FireOrNot(ByVal IfStatement1 As IfStatement) As Boolean
+        If IfStatements.Count = 0 Then
+            Return False
+        Else
+            'NOTE: assume there is only one IfStatement in IfStatements (ArrayList)
+            Dim checkingAgainst As IfStatement = IfStatements.Item(0)
+            'if the input ifstatement is same as the one checking against it -> Fire
+
+            Dim keys() As String = checkingAgainst.HashTable1.Keys
+            For i As Integer = 0 To keys.Length
+                If IfStatement1.HashTable1.ContainsKey(keys(i)) = False Then
+                    Return False
+                End If
+
+                Dim IfStatementOriginal As IfStatement = IfStatements(0)
+
+                'NOTE: must be completely correct - all conditions in the IfStatement must be the same/satisfied
+                If IfStatement1.HashTable1(keys(i)) = Not IfStatementOriginal.HashTable1(keys(i)) Then
+                    Return False
+                End If
+            Next
+
+            Return True
+        End If
+    End Function
+End Class
