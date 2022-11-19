e4fbc62ce8c53c5249340c2eb859ba9ae327c721
Dennis Gahm <dennisgahm@hotmail.com>
2022-11-19 20:15:49 +1000

    a little bit more progress

--- Neural Net Explorations 3/Form1.vb
+++ Neural Net Explorations 3/Form1.vb
@@ -2,6 +2,8 @@
 
     'Size of game is 10x10
     Dim ballLocation() As Integer
+    Dim ballDirection As Integer
+    Dim ballDown As Boolean
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         'ballLocation = {0, 0} 'Top-left; first 0 => horizontal position
         Randomize()
@@ -12,12 +14,25 @@
         'set direction
         '-2 -1 0 1 2
         'very left, left, down, ...
-        Dim value2 As Integer = CInt(Int(5 * Rnd())) '0 to 4
+        ballDirection = CInt(Int(5 * Rnd())) '0 to 4
+
+        'Ball is going down
+        ballDown = True
 
         'Experiment: generates the number from 0 to 4
         'For i As Integer = 0 To 100
         '    RichTextBox1.AppendText(" " & CInt(Int(5 * Rnd())))
         'Next
+
+
+
+    End Sub
+
+    Private Sub GenerateNextFrame()
+        'Move ballLocation in the direction of direction
+
+        'Eventually got to check for ball collision with pong-bar
+        'Got to also check for: hitting sides
 
     End Sub
 End Class
