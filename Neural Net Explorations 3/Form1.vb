46c6861dc45b8db28dfbe1840c7b4cd891df09bc
Dennis Gahm <dennisgahm@hotmail.com>
2022-11-19 20:01:24 +1000

    commit

--- Neural Net Explorations 3/Form1.vb
+++ Neural Net Explorations 3/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
 
+    'Size of game is 10x10
+    Dim ballLocation() As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'ballLocation = {0, 0} 'Top-left; first 0 => horizontal position
+        Randomize()
+        Dim value As Integer = CInt(Int((9 * Rnd())))
+
+        ballLocation = {value, 0}
+
+        'set direction
+        '
+        Dim value2 As Integer = CInt(Int((5) * Rnd())))
+
+    End Sub
 End Class
